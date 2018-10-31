6758cbeb2584cb39df0efc1e2978268f838ef965
AbaDV <39341297+AbaDV@users.noreply.github.com>
2018-11-01 00:31:14 +0300

    Рефакторинг: убран костыль в виде атрибута direction + для стрелок навигации назначены отдельные функции + упрощен код метода changeBigPicture()

--- js1_lesson6project_slider/js/scripts.js
+++ js1_lesson6project_slider/js/scripts.js
@@ -1,81 +1,37 @@
 const images = document.querySelectorAll(".img");
 let currentImageIndex = 0;
 
-
-function changeBigPicture(e) {
-    console.log('[e.target]', e.target);
-    const direction = getDirection(e);
-    console.log('[direction]: ', direction);
+function changeBigPicture(imgElement) {
+    let nextPictureSrc = imgElement.getAttribute("src");
+    nextPictureSrc = images[currentImageIndex].src;
 
     let currentBigPicture = document.getElementById("big_picture");
-    console.log('[currentBigPicture]: ', currentBigPicture);
-    if (direction === 'forward') {
-        currentImageIndex++;
-        if (currentImageIndex >= images.length) {
-            currentImageIndex = 0;
-        }
-    } else if (direction === 'back') {
-        currentImageIndex--;
-        if (currentImageIndex < 0) {
-            currentImageIndex = images.length - 1;
-        }
-    }
-
-    let nextPictureSrc = e.target.getAttribute("src");
-    nextPictureSrc = images[currentImageIndex].src;
     currentBigPicture.src = nextPictureSrc;
-    console.log(currentBigPicture.src);
 }
 
 let sliderArrowForward = document.querySelector(".slider-arrow-forward");
 let sliderArrowBack = document.querySelector(".slider-arrow-back");
 
-// function goForward(e) {
-//     if (currentImageIndex > images.length) {
-//         currentImageIndex = 0;
-//     }
-// }
-// function goBack(e) {
-//     if (currentImageIndex < 0) {
-//         currentImageIndex = images.length - 1;
-//     }
-// }
-
-
-function init() {
-    // sliderArrowForward.onclick = function (e) {
-    //     console.log("Кликнули на стрелку Вперед");
-    // }
-
-    // sliderArrowBack.onclick = function () {
-    //     console.log("Кликнули на стрелку Назад");
-    // }
-
-    sliderArrowForward.addEventListener("click", changeBigPicture);
-    sliderArrowBack.addEventListener("click", changeBigPicture);
-
-    // for (let i = 0; i < images.length; i++) {
-    //     const bigPicture = {
-    //         target: images[i]
-    //     }
-    //     changeBigPicture(bigPicture);
-    // }
-    changeBigPicture({ target: images[currentImageIndex] });
-
-    currentImage = document.getElementById('big_picture');
-    console.log('[currentImage]', currentImage);
+function goForward(e) {
+    currentImageIndex++;
+    if (currentImageIndex >= images.length) {
+        currentImageIndex = 0;
+    }
+    changeBigPicture(images[currentImageIndex]);
+}
+function goBack(e) {
+    currentImageIndex--;
+    if (currentImageIndex < 0) {
+        currentImageIndex = images.length - 1;
+    }
+    changeBigPicture(images[currentImageIndex]);
 }
 
-function getDirection(e) {
-    let direction;
-    const clickedElement = e.target;
-    if (clickedElement.nodeName === 'path') {
-        direction = clickedElement.parentElement.getAttribute('direction');
-    }
-    if (clickedElement.nodeName === 'svg') {
-        direction = clickedElement.getAttribute('direction');
-    }
-    return direction;
+function init() {
+    sliderArrowForward.addEventListener("click", goForward);
+    sliderArrowBack.addEventListener("click", goBack);
+
+    changeBigPicture(images[currentImageIndex]);
 }
 
 window.onload = init;
