9cec0b6d360d96e5bdb16a7f09049d8b9c832281
AbaDV <39341297+AbaDV@users.noreply.github.com>
2019-01-27 12:24:03 +0300

    Отдельная папка для урока 3

---
+++ react-lesson3/src/Login.js
@@ -0,0 +1,9 @@
+class Login extends React.Component {    
+    render() {
+        return <div>
+            <h3>Login</h3>
+        </div>
+    }
+}
+
+exports.Login = Login
---
+++ react-lesson3/src/index.jsx
@@ -0,0 +1,4 @@
+import { Developer } from "./developer";
+
+let developer1 = new Developer('Jhon', 26);
+console.log(developer1.displayInfo());
