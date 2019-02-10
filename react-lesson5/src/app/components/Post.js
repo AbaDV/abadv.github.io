98fc1842784bfe46c9f4fc8e5127afe48583537f
AbaDV <39341297+AbaDV@users.noreply.github.com>
2019-02-10 13:06:02 +0300

    урок 5

---
+++ react-lesson5/src/app/components/Post.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Post extends React.Component {
+    render() {
+        const { post } = this.props;
+
+        return <div>
+            <h4>{post.title}</h4>
+            <p>{post.text}</p>
+        </div>;
+    }
+}
+
+Post.propTypes = {
+    post: PropTypes.shape({
+        title: PropTypes.string,
+        text: PropTypes.string,
+    }),
+}
+
+export default Post;
